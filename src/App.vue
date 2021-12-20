109fd57baa9d59ab212a918c87db7b7f1b5712cc
Devessier <baptiste@devessier.fr>
2021-12-20 19:53:55 +0100

    feat: set up Vue 3 TS project with Vite

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import './index.css'
+</script>
+
+<template>
+  <main class="flex justify-center items-center min-h-screen">
+    <h1 class="">Vitest + Testing Library Vue</h1>
+  </main>
+</template>
